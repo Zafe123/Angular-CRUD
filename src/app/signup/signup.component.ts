4c30243848eb45423caf884d0890db0451ba6100
Zafe123 <scottpilgrim712@gmail.com>
2023-04-19 22:55:48 +0800

    Signup 10%

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+  public signupForm !: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this._formBuilder.group({
+      fullname: [''],
+      email: [''],
+      password: [''],
+      confirmpassword: ['']
+    })
+  }
+  hide = true;
+
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  signup() {
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
+      .subscribe(res => {
+        alert("Signup Successfull");
+        this.signupForm.reset();
+        this.router.navigate(['login']);
+      }, err => {
+        alert("Something went Wrong")
+      })
+  }
 }
