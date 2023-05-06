1a799ed5b528af41f63502660772714d598ebf60
Zafe123 <scottpilgrim712@gmail.com>
2023-05-06 15:15:10 +0800

    userinfo 20%

--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -11,15 +11,24 @@ import Swal from 'sweetalert2';
   styleUrls: ['./userinfo.component.scss']
 })
 export class UserinfoComponent implements OnInit {
-  hide: any;
-  email: any;
-  confirmpassword: any;
-  signupForm: any;
 
-  constructor() {
+  userFullName: string = '';
+
+
+  constructor(private http: HttpClient) {
+    const userId = localStorage.getItem('userId');
+    const usersUrl = 'http://localhost:3000/signupUsers';
+
+    this.http.get<any[]>(usersUrl).subscribe(users => {
+      const currentUser = users.find(user => user.id === Number(userId));
+      if (currentUser) {
+        this.userFullName = currentUser.fullname;
+      }
+    });
   }
 
   ngOnInit(): void {
+
 
   }
 
