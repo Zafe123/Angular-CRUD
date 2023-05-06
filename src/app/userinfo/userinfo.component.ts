26417708a78a024c730d119b2a403ae27f2b8b51
Zafe123 <scottpilgrim712@gmail.com>
2023-05-06 14:59:57 +0800

    Userinfo 10%

--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-userinfo',
   templateUrl: './userinfo.component.html',
   styleUrls: ['./userinfo.component.scss']
 })
-export class UserinfoComponent {
+export class UserinfoComponent implements OnInit {
+  hide: any;
+  email: any;
+  confirmpassword: any;
+  signupForm: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+
+  successNotification() {
+    Swal.fire('Hi', 'Signup Successful', 'success');
+  }
 
 }
